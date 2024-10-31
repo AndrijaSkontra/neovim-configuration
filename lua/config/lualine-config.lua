0128ec2ea14772e308493549e79d247818a99a97
Andrija Skontra <askontra22@student.unizd.hr>
2024-10-31 14:17:40 +0100

    noice is back now without bugs and new lualine

--- lua/config/lualine-config.lua
+++ lua/config/lualine-config.lua
@@ -1,6 +1,4 @@
 local lualine = require("lualine")
 lualine.setup({
-  options = { theme = "horizon" }, --  INFO: SET THEME HERE
+  options = { theme = "codedark" }, --  INFO: SET THEME HERE
 })
-
-vim.opt.cmdheight = 0 --  WARN: NEED THIS SO THE LINE IS ON THE BOTTOM OF SCREEN
--- lua/config/plugins/noice.lua
+++ lua/config/plugins/noice.lua
@@ -2,14 +2,16 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    -- add any options here
+    -- this is required to show the @recording... for macro
+    routes = {
+      {
+        view = "notify",
+        filter = { event = "msg_showmode" },
+      },
+    },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
   },
 }
