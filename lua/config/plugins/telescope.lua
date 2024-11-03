800f52011d6372130fe4db5d831dd87b45211b35
Andrija Skontra <askontra22@student.unizd.hr>
2024-11-03 21:00:32 +0100

    test: telescope quickfix

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -4,7 +4,24 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local tb = require("telescope.builtin")
+    function OpenQuickfixForLspDiagnostic()
+      local diagnostics = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
+      if #diagnostics == 0 then
+        print("No diagnostic under cursor")
+        return
+      end
+
+      -- Populate quickfix list with current buffer diagnostics
+      vim.diagnostic.setqflist({ open = true })
+    end
+
     vim.api.nvim_set_keymap("n", "<leader>tt", "<CMD>Themery<CR>", { desc = "Change theme" })
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>qf",
+      ":lua OpenQuickfixForLspDiagnostic()<CR>",
+      { noremap = true, silent = true }
+    )
     vim.api.nvim_set_keymap("n", "<leader>ff", "<CMD>Telescope smart_open<CR>", { desc = "Find file" })
     vim.api.nvim_set_keymap("n", "<leader>sg", "<CMD>Telescope live_grep<CR>", {})
     vim.api.nvim_set_keymap("n", "<leader>sk", "<CMD>Telescope keymaps<CR>", {})
