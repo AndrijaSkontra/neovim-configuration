b01f3440245d111836344d90f610a0c44980b295
AndrijaSkontra <andrija.skontra@kodelab.hr>
2024-12-24 13:03:06 +0100

    change: dadbod config

--- lua/config/plugins/dadbod.lua
+++ lua/config/plugins/dadbod.lua
@@ -1,7 +1,14 @@
 return {
   "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-ui",
-  keys = {
-    { "<leader>db", "<CMD>DBUIToggle<CR>", desc = "Toggle [D]ata[B]ase" },
+  dependencies = {
+    {
+      "kristijanhusak/vim-dadbod-ui",
+      keys = {
+        { "<leader>du", "<CMD>DBUIToggle<CR>", desc = "Toggle [D]ata[B]ase" },
+      },
+    },
   },
+  init = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
 }
