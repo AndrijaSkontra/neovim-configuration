335d0e515d25fac190f798b80b7396742ba39d79
AndrijaSkontra <andrija.skontra@kodelab.hr>
2025-01-20 15:05:25 +0100

    change: autocomplete now max 20 autocompletes recommended

--- lua/config/plugins/autocomplete.lua
+++ lua/config/plugins/autocomplete.lua
@@ -94,11 +94,15 @@ return { -- Autocompletion
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       }),
+      -- sources = {
+      --   { name = "nvim_lsp" },
+      --   { name = "luasnip" },
+      --   { name = "path" },
+      -- },
       sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-        -- { name = 'codeium' },
+        { name = "nvim_lsp", max_item_count = 20 },
+        { name = "luasnip", max_item_count = 20 },
+        { name = "path", max_item_count = 20 },
       },
     })
   end,
